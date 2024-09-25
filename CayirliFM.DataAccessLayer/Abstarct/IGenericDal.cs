b9496dd7d25657c0bdcc133f8f5521d387393568
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-09-25 12:03:41 +0300

    CayirliFM Radyo Projesi
    Bölüm 8 - Repository Design Pattern

---
+++ CayirliFM.DataAccessLayer/Abstarct/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CayirliFM.DataAccessLayer.Abstarct
+{
+	public interface IGenericDal<T> where T : class
+	{
+		void Craete(T t);
+		void Update(T t);
+		void Delete(int id);
+		Task<List<T>> GetListAll();
+		Task<T> GetById(int id);
+	}
+}
---
+++ CayirliFM.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using CayirliFM.DataAccessLayer.Abstarct;
+using CayirliFM.DataAccessLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CayirliFM.DataAccessLayer.Repositories
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		private readonly Context _context;
+
+		public GenericRepository(Context context)
+		{
+			_context = context;
+		}
+
+		public void Craete(T t)
+		{
+			_context.Add(t);
+			_context.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			_context.Remove(id);
+			_context.SaveChanges();
+		}
+
+		public async Task<T> GetById(int id)
+		{
+			return await _context.Set<T>().FindAsync(id);
+		}
+
+		public async Task<List<T>> GetListAll()
+		{
+			return await _context.Set<T>().ToListAsync();
+		}
+
+		public void Update(T t)
+		{
+			_context.Update(t);
+			_context.SaveChanges();
+		}
+	}
+}
