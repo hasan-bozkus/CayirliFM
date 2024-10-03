dc97e09f6696e72f345896acb39d60ab2400f60b
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-03 12:07:58 +0300

    CayirliFM Radyo Projesi
    Bölüm 11 - Servicelerin Oluşuturulması

---
+++ CayirliFM.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CayirliFM.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void TCraete(T t);
+        void TUpdate(T t);
+        void TDelete(int id);
+        Task<List<T>> TGetListAll();
+        Task<T> TGetById(int id);
+    }
+}
