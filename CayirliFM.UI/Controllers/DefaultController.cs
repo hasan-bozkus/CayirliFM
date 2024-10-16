e3a1e1483a1d8057869315558b75710a878acc7c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-16 12:34:45 +0300

    CayirliFM Radyo Projesi
    Bölüm 14 - Default Index Sayfası

---
+++ CayirliFM.UI/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CayirliFM.UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WelcomePartial()
+        {
+            return PartialView();
+        }
+
+        public IActionResult EventPartial()
+        {
+            return PartialView();
+        }
+
+        public IActionResult AlbumsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
