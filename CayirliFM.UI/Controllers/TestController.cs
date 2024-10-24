46cc2c3cc0c1f372ee681d276cb2d35c3a442272
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-24 14:00:27 +0300

    CayirliFM Radyo Projesi
    Bölüm 17 - Test Controller 1

---
+++ CayirliFM.UI/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using CayirliFM.BusinessLayer.Abstract;
+using CayirliFM.EntityLayer.Contrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CayirliFM.UI.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public TestController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _categoryService.TGetListAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTest(Category category)
+        {
+            category.CategoryCreatedAtTime = DateTime.Parse(DateTime.Now.ToString());
+            category.CategoryUpdatedAtTime = DateTime.Parse(DateTime.Now.ToString());
+            category.CategoryStatus = true;
+            _categoryService.TCraete(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- CayirliFM.UI/Program.cs
+++ CayirliFM.UI/Program.cs
@@ -1,4 +1,18 @@
+using CayirliFM.BusinessLayer.Abstract;
+using CayirliFM.BusinessLayer.Concrete;
+using CayirliFM.DataAccessLayer.Abstarct;
+using CayirliFM.DataAccessLayer.Concrete;
+using CayirliFM.DataAccessLayer.EntityFramework;
+using CayirliFM.DataAccessLayer.Repositories;
+using CayirliFM.EntityLayer.Contrete;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IGenericDal<Category>, GenericRepository<Category>>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
