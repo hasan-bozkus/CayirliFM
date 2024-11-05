8ba51be4be0735a8f2ef5b3a73eb0e8e9261c08d
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-11-05 12:05:19 +0300

    CayirliFM Radyo Projesi
    Bölüm 19 - Admin Dashboard 1

---
+++ CayirliFM.UI/ViewComponents/Admin/AdminLayout/_HeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CayirliFM.UI.ViewComponents.Admin.AdminLayout
+{
+    public class _HeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ CayirliFM.UI/ViewComponents/Admin/AdminLayout/_SideBarPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CayirliFM.UI.ViewComponents.Admin.AdminLayout
+{
+    public class _SideBarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
